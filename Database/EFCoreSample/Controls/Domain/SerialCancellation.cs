2239924161c0b96aad68e4b6020f5a1dced944cc
kingston <kingstonkuan@u.nus.edu>
2020-06-05 15:53:24 +0800

    implement serial writing using background tasks

---
+++ Database/EFCoreSample/Controls/Domain/SerialCancellation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace EFCoreSample.Controls.Domain
+{
+    public class SerialCancellation : ISerialCancellation
+    {
+        private readonly ConcurrentDictionary<Guid, CancellationTokenSource> _sources;
+
+        public SerialCancellation()
+        {
+            _sources = new ConcurrentDictionary<Guid, CancellationTokenSource>();
+        }
+
+        public void AddSource(Guid id, CancellationTokenSource source)
+        {
+            var added = false;
+            while (!added)
+            {
+                added = _sources.TryAdd(id, source);
+                if (!added) RemoveSource(id);
+            }
+        }
+
+        public void RemoveSource(Guid id)
+        {
+            var exists = _sources.TryGetValue(id, out var source);
+            if ( exists && !source.Token.IsCancellationRequested) source.Cancel();
+            var removed = _sources.TryRemove(id, out source);
+            if (removed) source.Dispose();
+        }
+    }
+}
---
+++ Database/EFCoreSample/Controls/Domain/SerialWrite.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using EFCoreSample.Controls.Repositories;
+using EFCoreSample.Monitoring.Domain;
+
+namespace EFCoreSample.Controls.Domain
+{
+    public class SerialWrite : ISerialWrite
+    {
+        private readonly IPort _port;
+        private readonly ICommandRepository _repo;
+
+        public SerialWrite(IPort port, ICommandRepository repo)
+        {
+            _port = port;
+            _repo = repo;
+        }
+
+        public async Task<Command> Write(Actuator actuator, Command command, CancellationTokenSource writeSource)
+        {
+            Console.WriteLine("Start timer: " + (command.Value.ExecuteDate - DateTime.Now));
+            await Task.Delay(command.Value.ExecuteDate - DateTime.Now);
+
+            // Check writeSource token
+            if (writeSource.Token.IsCancellationRequested)
+            {
+                Console.WriteLine("Command was cancelled");
+                writeSource.Dispose();
+                return command;
+            }
+            
+            var result = await _repo.Complete(command.Id);
+            if (result == null)
+            {
+                Console.WriteLine("Command not found");
+                return null;
+            }
+
+            // _port.Serial.WriteLine(actuator.Value.Position + "," + command.Value.Value + "," + command.Value.Units);
+            Console.WriteLine("Completed: " + result.Id);
+            return result;
+
+        }
+    }
+}

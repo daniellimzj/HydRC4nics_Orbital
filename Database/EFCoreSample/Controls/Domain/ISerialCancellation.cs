2ea7697bb2fb8283bd07f475c828eae13e310841
kingston <kingstonkuan@u.nus.edu>
2020-06-05 15:53:09 +0800

    interface for serial write of command through task

---
+++ Database/EFCoreSample/Controls/Domain/ISerialCancellation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading;
+
+namespace EFCoreSample.Controls.Domain
+{
+    public interface ISerialCancellation
+    {
+        void AddSource(Guid id, CancellationTokenSource source);
+        void RemoveSource(Guid id);
+    }
+}
---
+++ Database/EFCoreSample/Controls/Domain/ISerialWrite.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using EFCoreSample.Controls.Repositories;
+
+namespace EFCoreSample.Controls.Domain
+{
+    public interface ISerialWrite
+    {
+        Task<Command> Write(Actuator actuator, Command command, CancellationTokenSource writeSource);
+    }
+}

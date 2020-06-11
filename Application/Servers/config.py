de7ecc627469b1e8a43f289f3d7cb6af4d4009b9
kingston <kingstonkuan@u.nus.edu>
2020-06-11 15:52:12 +0800

    config environment variables

--- Application/Servers/config.py
+++ Application/Servers/config.py
@@ -1,1 +1,7 @@
-#Configuration for use of environment variables
+"""Central configuration"""
+import os
+
+BOT_TOKEN = os.environ['BOT_TOKEN']
+FLASK_SECRET = os.environ['FLASK_SECRET']
+DB_URL = os.environ['DB_URL']
+JWT_SECRET = os.environ['JWT_SECRET']
